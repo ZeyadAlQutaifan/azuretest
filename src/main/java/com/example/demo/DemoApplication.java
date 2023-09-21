e315d22674bf7d1a299a8fd42f259b8446b03415
zeyad alqutaifan <zeyada.alqutaifan@gmail.com>
2023-09-21 15:47:19 +0100

    Deploy3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,26 +5,55 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
 
 @RestController("/greeting")
-class Test{
-	@GetMapping("/")
-	public String greeting(){
-		return "hello";
-	}
+class Test {
+    @GetMapping("/")
+    public String greeting() {
+        return "hello";
+    }
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "<h2> Hello Nadia\n This is the first web service in my project </h2>" +
-				"<br> <h2>I have decide to make the first end-point return Your name,</h2> " +
-				"<h1>LOVE YOU</h1>";
-	}
+    @GetMapping("/hello")
+    public String hello() {
+        return "<h2> Hello Nadia\n This is the first web service in my project </h2>" +
+                "<br> <h2>I have decide to make the first end-point return Your name,</h2> " +
+                "<h1>LOVE YOU</h1>";
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+		int id = 0 ;
+        List<User> users = new ArrayList<>();
+		users.add(new User(id ++ , "zeyad", "zeyad21kh", "zeyad@ze.com"));
+		users.add(new User(id ++ , "zeyad", "zeyad21kh", "zeyad@ze.com"));
+		users.add(new User(id ++ , "zeyad", "zeyad21kh", "zeyad@ze.com"));
+		users.add(new User(id ++ , "zeyad", "zeyad21kh", "zeyad@ze.com"));
+
+		return users;
+    }
+
+}
+
+class User {
+	private int id ;
+    private String name;
+    private String username;
+    private String email;
+    public User(int id , String name, String username, String email) {
+		this.id = id ;
+        this.name = name;
+        this.username = username;
+        this.email = email;
+    }
 }
